e4077bf3e11336268f1b6d9bd973e40e845fc63f
chrislag94 <chrislag94@gmail.com>
2023-07-30 01:06:17 +0400

    second commit

--- get_youtube_url.py
+++ get_youtube_url.py
@@ -1,9 +1,15 @@
 from youtubesearchpython import VideosSearch
 import sys
+import subprocess
+
+
 
 def get_video_link():
+    bash_command = "mpv "
     if len(sys.argv) > 1:
-        return VideosSearch(sys.argv[1], limit=1).result()['result'][0]['link']
+        bash_command += VideosSearch(sys.argv[1], limit=1).result()['result'][0]['link']
+        result = subprocess.run(bash_command, shell=True, capture_output=True, text=True)
+
     else:
         return "No input provided."
 
